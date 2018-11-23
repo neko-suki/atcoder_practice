754fd0120ab84daac0533bd28a887e768c2724b5
neko-suki <s1150092@gmail.com>
2018-11-23 12:27:18 +0900

    First compiled

---
+++ AGC017/B.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+typedef long long ll;
+
+string solve(int n, ll a, ll b, ll c, ll d){
+  for(int i = 0;i < n-1;i++){
+    ll l = a + i * c - (n-i-1) * c;
+    ll r = a + i * d - (n-i-1) * d;
+    if (l > r)swap(l , r);
+    if (l <= b && r <= b)return "YES";
+  }
+  return "NO";
+}
+
+int main(){
+  int n;
+  ll a, b, c, d;
+  cin >> n >> a>> b >> c>> d;
+  cout << solve(n, a, b, c, d) << endl;
+
+  return 0;
+}
