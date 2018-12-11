5e4bf3986266157b9c6184e9e395c8de2436f076
neko-suki <s1150092@gmail.com>
2018-12-11 23:32:35 +0900

    add return in function

--- ARC060/D.cpp
+++ ARC060/D.cpp
@@ -5,9 +5,11 @@ void solve(const string & in){
   for(int i = 0;i < in.size();i++){
     if (i + 1 < in.size() && in[i] == in[i+1]){
       cout << i+1 <<" " << i+2 << endl;
+      return;
     }
     if (i + 2 < in.size() && in[i] == in[i+2]){
       cout << i+1 <<" " << i+3 << endl;
+      return;
     }
   }
 
